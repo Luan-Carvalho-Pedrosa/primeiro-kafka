5141f54d4464c44ef1acebce8cfc37debc590f09
LuanPedrosa <desenvolve2@milnet.com.br>
2025-09-12 11:45:21 -0300

    integracoes

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ docker compose up -d --build (Opção para liberar o terminal)
 
 docker compose up --build (Opção para acompanhar o log do que está acontecendo durante a criação do container)
 ```
+
+### Parar e remover os containers caso precise:
+```bash
+docker compose down
+```
---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/ETopics.java
@@ -0,0 +1,24 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ETopics {
+
+    START_SAGA("start-saga"),
+    FINISH_SUCCESS("finish-succes"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
+    NOTIFY_ENDING("notify-ending"),
+    BASE_ORCHESTRATOR("orchestrator");
+
+    private String topic;
+    
+}
