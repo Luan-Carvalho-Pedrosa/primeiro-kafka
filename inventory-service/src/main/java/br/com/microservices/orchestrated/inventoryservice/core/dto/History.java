0f1106e1b9c8093885b0aaebdaae32840d01aa82
LuanPedrosa <desenvolve2@milnet.com.br>
2025-09-15 15:55:22 -0300

    dtos

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/History.java
@@ -0,0 +1,20 @@
+package br.com.microservices.orchestrated.inventoryservice.core.dto;
+
+import java.time.LocalDateTime;
+
+import br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+
+    private String source;
+    private ESagaStatus status;
+    private String message;
+    private LocalDateTime createdAt;
+    
+}
---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Order.java
@@ -0,0 +1,23 @@
+package br.com.microservices.orchestrated.paymentservice.core.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    private String id;
+    private List<OrderProducts> products;
+    private LocalDateTime createdeAt;
+    private String transactionId;
+    private double totalAmount;
+    private int totalItems;
+
+    
+}
